68ad3075c13ffdf472cfa267c08e46899f3ac892
staciahoov <staciaray@roadrunner.com>
2016-04-02 23:45:54 +0000

    progress on ball movement

--- script/main.js
+++ script/main.js
@@ -1,12 +1,13 @@
 (function() {  
     var canvas = document.getElementById("pongTable"); //DOM node in html
     var context = canvas.getContext("2d"); //object with methods & properties used to render graphics in canvas element.
-    context.fillStyle = "blue";
+    context.fillStyle = "#11B1FF";
     context.fillRect(300,300,600,400);
     
     var player = new Player();
     var computer = new Computer();
-    var ball = new Ball(500, 500);
+    var ball = new Ball(600, 500);
+    var keysDown = {};
     
     var animate = window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -23,6 +24,7 @@
     
     var update = function() {
         player.update();
+        ball.update();
     }     
         
     function step() { //
@@ -36,60 +38,28 @@
        this.y = y;
        this.width = width;
        this.height = height;
-       this.speed = 0;
-    };
-
-    function Player() {
-       this.paddle =  new Paddle(850, 465, 10, 70);
+       this.x_speed = 0;
+       this.y_speed = 0;
     };
     
-    function Computer() {
-        this.paddle = new Paddle(350, 465, 10, 70);
-    };
-    
-    function Ball(x, y, radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = 7;
-        this.x_speed = 0;
-        this.y_speed = 0;
-    };
-   
-    Ball.prototype.render = function() {
-        context.beginPath();
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false); //arc (x,y, radius, startAngle, anticlockwise?)
-        context.fillStyle = 'yellow';
-        context.fill();
-        context.lineWidth =2;
-        context.strokeStyle = '#003300';
-        context.stroke();
+    Paddle.prototype.render = function() {
+        context.fillStyle = "purple";
+        context.fillRect(this.x, this.y, this.width, this.height);
     };
     
     Paddle.prototype.move = function(x, y) {
         this.x += x;
         this.y += y;
-        this.speed += y
+        this.x_speed += x;
+        this.y_speed += y;
         if(this.y < 300) {
             this.y = 300;
-            this.speed = 0;
-        } else if ((this.y + this.height) > 700) {
+            this.y_speed = 0;
+        } else if (this.y + this.height > 700) {
             this.y = 700 - this.height;
-            this.speed = 0;
+            this.y_speed = 0;
         };
         console.log(this.x,this.y)
-    };
-    
-    Paddle.prototype.render = function() {
-        context.beginPath();
-        context.fillStyle = "purple";
-        context.fillRect(this.x, this.y, this.width, this.height);
-        context.lineWidth = 7;
-        context.strokeStyle = 'black';
-        context.stroke();
-    };
-    
-    Player.prototype.render = function() {
-        this.paddle.render();
     };
     
     Player.prototype.update = function() {
@@ -104,14 +74,63 @@
             }
         }
     };
+
+    function Player() {
+       this.paddle =  new Paddle(850, 465, 10, 70);
+    };
+    
+    Player.prototype.render = function() {
+        this.paddle.render();
+    };
         
+    function Computer() {
+        this.paddle = new Paddle(350, 465, 10, 70);
+    };
+    
     Computer.prototype.render = function() {
         this.paddle.render();
     };
-    
-    
-    var keysDown = {};
 
+    function Ball(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radius = 7;
+        this.x_speed = Math.random() * 10;
+        this.y_speed = Math.random() * 10;
+    };
+   
+    Ball.prototype.render = function() {
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false); //arc (x,y, radius, startAngle, anticlockwise?)
+        context.fillStyle = 'yellow';
+        context.fill();
+    };
+    
+    Ball.prototype.update = function() {
+        this.x += this.x_speed;
+        this.y += this.y_speed;
+        this.x_left = this.x - 3.5;
+        this.x_right = this.x + 3.5;
+        this.y_top = this.y - 3.5;
+        this.y_bottom = this.y + 3.5;
+        
+        if (this.x < 300 || this.x > 900) {
+            this.x = 600;
+            this.y = 500;
+            this.x_speed = 3;
+            this.y_speed = 0;
+        }
+        
+        if (this.y - 3.5 < 300) {
+            this.y = 303.5;
+            this.y_speed = -this.y_speed;
+        } else if (this.y + 3.5 > 700) {
+            this.y = 696.5;
+            this.y_speed = - this.y_speed;
+        }
+        
+        
+    };
+    
     window.addEventListener("keydown", function(event) {
        keysDown[event.keyCode] = true;
     });
